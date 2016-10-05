a3147e8dc6d06dba5421409141bcbbe6c5a8b42b
Neil Zimmerman <ntz@stsci.edu>
2016-10-05 10:07:20 -0400

    updated list processing parameters for routine wfsc

--- ote_targets_driver.py
+++ ote_targets_driver.py
@@ -10,7 +10,8 @@ import argparse
 
 from list_specs import target_lists
 
-def run_ote_target_pipeline(category, new_file_path, all_cores=False, max_final_length=10, min_per_day=3, min_per_day_hemi=3):
+def run_ote_target_pipeline(category, target_file_path, new_file_path, 
+                            all_cores=False, max_final_length=10, min_per_day=3, min_per_day_hemi=3):
     #///////////////////////////////////////////
     # BUILD TARGET LISTS
     #///////////////////////////////////////////
@@ -20,7 +21,10 @@ def run_ote_target_pipeline(category, new_file_path, all_cores=False, max_final_
         n_cores = multiprocessing.cpu_count()/2
     owd = os.getcwd()
     os.chdir(os.path.normpath(os.environ['OTE_TARGETS_PIPELINE']))
-    subprocess.check_output(['python2.7', 'list_builder.py', category, '--newfilepath', new_file_path, '--ncores', str(n_cores)])
+    subprocess.check_output(['python2.7', 'list_builder.py', category,
+                             '--targetfilepath', target_file_path,
+                             '--newfilepath', new_file_path,
+                             '--ncores', str(n_cores)])
     list_name = os.path.join(new_file_path, category, category)
     assert os.path.exists(list_name), 'Expected list_builder product does not exist.'
 
@@ -61,30 +65,40 @@ if __name__ == "__main__":
     assert "OTE_TARGETS" in os.environ, "Set the OTE_TARGETS shell variable to specify location of new target list products, otherwise provide an explicit path using the --newfilepath command line argument"
     assert "OTE_TARGETS_PIPELINE" in os.environ, "Set the OTE_TARGETS_PIPELINE shell variable to specify the path to the OTE target pipeline"
     parser = argparse.ArgumentParser(description="Top level driver script to produce lists of OTE commissioning target candidates.")
+    parser.add_argument("--targetfilepath", type=str,
+                        default=os.path.normpath(os.environ["OTE_TARGETS"]),
+                        help="Destination directory for new files.")
     parser.add_argument("--newfilepath", type=str,
                         default=os.path.join(os.path.normpath(os.environ["OTE_TARGETS"]),"target_lists_{:s}".format(datetime.datetime.now().strftime("%Y-%m-%d"))),
                         help="Destination directory for new files.")
     parser.add_argument("--allcores", default=False, help="Use all processor cores when running list_builder catalog queries.", action="store_true")
     args = parser.parse_args()
     
+    subprocess.call("mkdir -p {:s}".format(args.targetfilepath), shell=True)
     subprocess.call("mkdir -p {:s}".format(args.newfilepath), shell=True)
 
     #///////////////////////////////////////////
     # EARLY COMMISSIONING
     #///////////////////////////////////////////
-    run_ote_target_pipeline('early_comm', args.newfilepath, all_cores=args.allcores)
+    run_ote_target_pipeline('early_comm', args.targetfilepath, args.newfilepath, all_cores=args.allcores)
 
     #///////////////////////////////////////////
     # GLOBAL ALIGNMENT
     #///////////////////////////////////////////
-    run_ote_target_pipeline('global_alignment', args.newfilepath, all_cores=args.allcores, min_per_day_hemi=3)
+    run_ote_target_pipeline('global_alignment', args.targetfilepath, args.newfilepath, all_cores=args.allcores, min_per_day_hemi=3)
 
     #///////////////////////////////////////////
     # COARSE PHASING
     #///////////////////////////////////////////
-    run_ote_target_pipeline('coarse_phasing', args.newfilepath, all_cores=args.allcores, min_per_day_hemi=3)
+    run_ote_target_pipeline('coarse_phasing', args.targetfilepath, args.newfilepath, all_cores=args.allcores, min_per_day_hemi=3)
 
     #///////////////////////////////////////////
     # FINE PHASING
     #///////////////////////////////////////////
-    run_ote_target_pipeline('fine_phasing', args.newfilepath, all_cores=args.allcores, min_per_day_hemi=5)
+    run_ote_target_pipeline('fine_phasing', args.targetfilepath, args.newfilepath, all_cores=args.allcores, min_per_day_hemi=5)
+
+    #///////////////////////////////////////////
+    # ROUTINE WFS&C
+    #///////////////////////////////////////////
+    run_ote_target_pipeline('routine_wfsc', args.targetfilepath, args.newfilepath, all_cores=args.allcores, 
+                            min_per_day=99999, min_per_day_hemi=99999, max_final_length=99999)
